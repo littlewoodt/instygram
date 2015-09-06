1d68374b3103653297b564a931a5f18d4ee9b72c
Tamara Littlewood <you@example.com>
2015-09-06 20:55:21 +1000

    Pics working : )

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@
 #  content    :text
 #  created_at :datetime
 #  updated_at :datetime
+#  user_id    :integer
 #
 
 class Post < ActiveRecord::Base
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+u1 = User.create :name => 'Craig', :username => 'TheDaddy', :email => 'craig@gmail.com'
+u2 = User.create :name => 'Tash', :username => 'TheMommy', :email => 'tash@gmail.com'
+
+Post.destroy_all
+p1 = Post.create :title => 'HTML babies', :image => 'http://40.media.tumblr.com/tumblr_me9izrcMfd1rt7j2bo1_500.jpg', :content => 'This is some fake content blah blah'
+p2 = Post.create :title => 'Eat your veggies', :image => 'http://browncardigan.s3.amazonaws.com/imagesfarm/2015/qmjW09hdEGlMH1C.jpg', :content => 'This is some fake content about eating your veggies'
+
+u1.posts << p1
+u2.posts << p2
