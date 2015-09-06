1739e94e003d8afc5b887160d7832867bf694a3a
Tamara Littlewood <you@example.com>
2015-09-07 07:38:03 +1000

    Working crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-  	params.require(:post).permit(:title, :image, :content)
+  	params.require(:post).permit(:title, :image, :content, :user_id)
   end
 
 end
